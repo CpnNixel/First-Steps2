ebc0ff35808b7a7eca21e61c5ba4ee668be85e5a
CpnNixel <44145910+CpnNixel@users.noreply.github.com>
2020-02-26 02:32:21 +0200

    first commit

---
+++ oop2/recursive singly linked list/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first element to create a recursive singly linked list\n");
+
+            RList kek = new RList(Convert.ToInt32(Console.ReadLine()));
+           
+            while (true)
+            {
+                ShowMenu();
+                int x = Convert.ToInt32(Console.ReadLine());
+               
+                
+                switch (x)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        Console.WriteLine();
+                        kek.Print(kek);
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("\n Enter the element you wanna insert");
+                        kek.Add(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 3:
+                        Console.WriteLine("List looks like this:");
+                        kek.Print(kek);
+                        Console.WriteLine("\n Enter the postion you wanna insert");
+                        int pos = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the number you wanna insert");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        kek.AddAtRec(pos, num, kek);
+                        break;
+                    case 4:
+                        Console.WriteLine("\n Enter the position you wanna delete");
+                        int pos2 = Convert.ToInt32(Console.ReadLine());
+                        kek.DeleteAtRec(pos2, kek); 
+                        break;
+                    case 5:
+                        kek.DeleteAbleByTwo(kek);
+                        break;
+                    case 6:
+                        Console.WriteLine();
+                        kek.RecInvPrint1(kek);
+                        Console.WriteLine();
+                        kek.RecInvPrint2(kek);
+                        break;
+                    case 7:
+                        Console.WriteLine("\nEnter the key you wanna get link to");
+                        int key = Convert.ToInt32(Console.ReadLine());
+                        kek.Print(kek.SearchByKey(key, kek));
+                        break;
+                    case 8:
+                        kek.Print(kek);
+                        Console.WriteLine("\nEnter element FROM");
+                        int _from = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("\nEnter element TO");
+                        int _to = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("The minimal number is");
+                        Console.WriteLine(kek[_from, _to, kek]);
+                        break;
+                }
+
+
+                //1,3 , 5, 8, 16, 24, 35, 40, 55
+            }
+            
+        }
+        //5. Не рекурсивний метод додавання нового елемента останнім у список;
+        //8. Рекурсивний метод додавання нового елемента n-ним у список;
+        //16. Рекурсивний метод видалення n-ного за рахунком елемента;
+        //24. Метод видалення всіх парних за значенням елементів;
+        //35. Рекурсивний метод друку елементів списку у зворотному порядку у стовпець;
+        //40. Метод пошуку елемента із заданим значенням (результат - посилання на знайдений елемент);
+        //55. Індексатор з двома параметрами, що дозволяє знайти мінімальне значення
+        //    серед значень елементів, розташованих між елементами із заданими номерами.
+        static void ShowMenu()
+        {
+            Console.WriteLine("\nPress 1 to Print the list");
+            Console.WriteLine("Press 2 to execute 5. \n \t\tНе рекурсивний метод додавання нового елемента останнiм у список;");
+            Console.WriteLine("Press 3 to execute 8. \n \t\tРекурсивний метод додавання нового елемента n-ним у список");
+            Console.WriteLine("Press 4 to execute 16. \n \t\tРекурсивний метод видалення n-ного за рахунком елемента");
+            Console.WriteLine("Press 5 to execute 24. \n \t\tМетод видалення всiх парних за значенням елементiв");
+            Console.WriteLine("Press 6 to execute 35. \n \t\tРекурсивний метод друку елементiв списку у зворотному порядку у стовпець");
+            Console.WriteLine("Press 7 to execute 40. \n \t\tМетод пошуку елемента iз заданим значенням (результат - посилання на знайдений елемент");
+            Console.WriteLine("Press 8 to execute 55. \n \t\tІндексатор з двома параметрами, що дозволяє знайти мiнiмальне значення\n\t\tсеред значень елементiв, розташованих мiж елементами iз заданими номерами.");
+            Console.WriteLine("Press 0 to exit");
+        }
+    }
+}
---
+++ oop2/recursive singly linked list/RList.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop2
+{
+    class RList
+    {
+        public int data;
+        public RList next;
+
+        public RList(int i, RList n)
+        {
+            data = i;
+            next = n;
+        }
+        public RList(int i)
+        {
+            data = i;
+            next = null;
+        }
+        public RList(RList n)
+        {
+            data = n.data;
+            next = n.next;
+        }
+        public RList()
+        {
+            data = default(int);
+            next = null;
+        }
+
+            //add last
+        public void Add(int num)
+        {
+            if (next == null)
+                next = new RList(num);
+            else
+            {
+                RList kek = next;
+
+                while (kek.next != null)
+                    kek = kek.next;
+                kek.next = new RList(num);
+            }
+        }
+        //подсчет кол-ва элементов в листе
+        public static int Counter(RList rl)
+        {
+            int count = 1;
+            while (rl.next != null)
+            {
+                count++;
+                rl = rl.next;
+            }
+            return count;
+        }
+        //8. Рекурсивний метод додавання нового елемента n-ним у список;
+        public void AddAtRec(int pos, int num,RList rl)
+        {
+            if(pos<1 || pos > Counter(rl))
+            {
+                throw new Exception("Введенная позиция меньше 1 или больше кол-ва элементов листа");
+            }
+            if (pos == Counter(rl))
+            {
+                while (rl.next != null)
+                {
+                    rl = rl.next;
+                }
+                rl.next = new RList(num);
+                return;
+            }
+            if(pos == 1)
+            {
+                int temp = rl.data;
+                rl.data = num;
+                RList lel = new RList(temp);
+                lel.next = rl.next;
+                rl.next = lel;
+            }
+            else
+            {
+                pos--;
+                AddAtRec(pos, num, rl.next);
+            }
+        }
+        //16. Рекурсивний метод видалення n-ного за рахунком елемента;
+        public void DeleteAtRec(int pos, RList rl)
+        {
+            //проверка
+            if (pos < 1 || pos > Counter(rl))
+            {
+                throw new Exception("Введенная позиция меньше 1 или больше кол-ва элементов листа");
+            }
+            //последний элемент
+            if (pos == Counter(rl))
+            {
+                while (rl.next.next != null)
+                {
+                    rl = rl.next;
+                }
+                rl.next = null;
+                return;
+            }
+            //первый элемент
+            if (pos == 1)
+            {
+                while (rl.next.next != null)
+                {
+                    rl.data = rl.next.data;
+                    rl = rl.next;
+                }
+                rl.data = rl.next.data;
+                rl.next = null;
+            }
+            else
+            {
+                pos--;
+                DeleteAtRec(pos, rl.next);
+            }
+        }
+        //24. Метод видалення всіх парних за значенням елементів;
+        public RList DeleteAbleByTwo(RList rl)
+        {
+            RList kek = rl;
+            RList prev = null;
+
+            if (kek == null)
+            {
+                return prev;
+            }
+            if (kek.next == null)
+            {
+                if (kek.data % 2 == 0)
+                {
+                    return null;
+                }
+            }
+            while (kek != null)
+            {
+                if (kek.data % 2 == 0)
+                {
+                    if (prev == null)
+                    {
+                        rl = rl.next;
+                        kek = rl;
+                    }
+                    else
+                    {
+                        prev.next = kek.next;
+                        kek = kek.next;
+                    }
+                }
+                else
+                {
+                    prev = kek;
+                    kek = kek.next;
+                }
+            }
+            return rl;
+        }
+
+        //35. Рекурсивний метод друку елементів списку у зворотному порядку у стовпець;
+        //принт начиная с первого
+        public void RecInvPrint1(RList rl)
+        {
+            if (rl.next == null)
+            {
+                Console.WriteLine(rl.data);
+            }
+            else
+            {
+
+                Console.WriteLine(rl.data);
+                RecInvPrint1(rl.next);
+            }
+        }
+        //принт начиная с последнеого
+        public void RecInvPrint2(RList rl)
+        {
+            if (rl.next == null)
+            {
+                Console.WriteLine(rl.data);
+            }
+            else
+            {
+                RecInvPrint2(rl.next);
+                Console.WriteLine(rl.data);
+            }
+        }
+        //40. Метод пошуку елемента із заданим значенням(результат - посилання на знайдений елемент);
+        public RList SearchByKey(int key, RList rl)
+        {
+            while (rl.next!=null)
+            {
+                if (rl.data == key)
+                {
+                    return rl;
+                }
+                rl = rl.next;
+            }
+            return rl;
+        }
+        public static void deleteEl(RList rl)
+        {
+            while (rl.next.next != null)
+            {
+                rl.data = rl.next.data;
+                rl = rl.next;
+            }
+            rl.data = rl.next.data;
+            rl.next = null;
+        }
+        //55.Індексатор з двома параметрами, що дозволяє знайти мінімальне значення
+        //серед значень елементів, розташованих між елементами із заданими номерами.
+
+        public int this[int i, int j, RList rl]
+        {
+            get
+            {
+                int count = 0;
+                int cou = 0;
+                //обрезаем конец листа
+                RList temp = CutJ(j-1, rl);
+                //обрезаем начало листа
+                temp = Cut(i, temp);
+                //находим минимальое число в промежутке
+                return finmin(temp);
+            }
+        }
+        
+        
+
+        
+        public  static RList Cut(int i, RList rl)
+        {
+
+            int count = 0;
+            while (rl.next != null)
+            {
+                count++;
+                if (count == i)
+                {
+                    return new RList(rl.next);
+                }
+                rl = rl.next;
+            }
+            return new RList();
+
+        }
+        public static RList CutJ(int j, RList rl)
+        {
+            int count = 0;
+            RList temp = new RList(rl.data);
+            while (count != j)
+            {
+                count++;
+                RList lul = new RList(rl.next.data);
+                lul.next = temp;
+                temp = lul;
+                rl = rl.next;
+            }
+
+            return Reverse(temp);
+        }
+        public static RList Reverse(RList rl)
+        {
+            dynamic n = null;
+            while (rl != null)
+            {
+                RList temp = rl.next;
+                rl.next = n;
+                n = rl;
+                rl = temp;
+            }
+            rl = n;
+            
+            return rl;
+
+        }
+      
+        
+        public static int finmin(RList LR)
+        {
+            int min = 10000;
+            while (LR.next != null)
+            {
+                if (LR.data < min)
+                {
+                    min = LR.data;
+                }
+                LR = LR.next;
+            }
+            //на случай если последний элемент наименьший
+            if (LR.data < min)
+            {
+                min = LR.data;
+            }
+            return min;
+        }
+
+        //1,3 , 5, 8, 16, 24, 35, 40, 55
+        public void Print(RList kek)
+        {
+            Console.WriteLine();
+            if (kek.next == null)
+            {
+                Console.Write(" "+kek.data+" ");
+            }
+            else
+            {
+                Print(kek.next);
+                Console.Write(" "+kek.data+" ");
+            }
+
+        }
+    }
+}
